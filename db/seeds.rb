01a7f4862bf49d773bee530a2903252aa8fc53bf
Travis Brewer <t.brewer.co@gmail.com>
2021-07-22 12:40:43 -0600

    updates chosen_answers POST contoller to res w chosen_answers

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,5 +63,8 @@ end
 
 
 ChosenAnswer.create(survey_question_id: 1, answer_option_id: 1, user_id: 1)
+ChosenAnswer.create(survey_question_id: 1, answer_option_id: 2, user_id: 5)
+ChosenAnswer.create(survey_question_id: 3, answer_option_id: 1, user_id: 4)
+ChosenAnswer.create(survey_question_id: 5, answer_option_id: 3, user_id: 4)
 ChosenAnswer.create(survey_question_id: 2, answer_option_id: 2, user_id: 1)
-ChosenAnswer.create(survey_question_id: 2, answer_option_id: 2, user_id: 1)
+ChosenAnswer.create(survey_question_id: 2, answer_option_id: 2, user_id: 2)
