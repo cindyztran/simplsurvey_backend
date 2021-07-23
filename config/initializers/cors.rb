5df62c508f95f5ca41a34bdf147dda84316f505e
cindytran <cindyztran@gmail.com>
2021-07-23 11:37:48 -0700

    reconfigures CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000, https://simplsurvey-frontend.herokuapp.com'
+    origins '*'
 
     resource '*',
       headers: :any,
